a34238648372d8e740d57a20254c2424d758061a
victor.druc <druc92@gmail.com>
2022-02-12 14:04:24 +0200

    init commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+import ProductCarousel from "../components/Product/ProductCarousel";
+
+const Index = () => (
+    <>
+      <Header/>
+      <ProductCarousel/>
+    </>
+);
+
+export default Index;
